80402face4cdb6ca484d106df41b54b56913adbd
maangeloatienza <maangeloatienza@gmail.com>
2020-05-05 16:05:08 +0800

    fix: endpoint issues

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {getToken} from './Commons';
 
 export default axios.create({
-    baseURL: "http://54.179.163.208:2424/api/",
+    baseURL: "http://54.179.163.208:2424/api/v1/",
     // baseURL: "http://localhost:8000/api/v1/",
     headers : {
       'Authorization' : `${getToken()}`
